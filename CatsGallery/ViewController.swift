ffad987329c06f1649150d52a021a4f22212bce6
Matheus Cunha <matheuspcunha@hotmail.com>
2020-06-10 18:18:57 -0300

    Initial Commit

---
+++ CatsGallery/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  CatsGallery
+//
+//  Created by Matheus Cunha on 10/06/20.
+//  Copyright © 2020 Matheus Cunha. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
